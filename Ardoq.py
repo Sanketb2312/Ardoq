23f0ed9a347373ef7566238dd9682e3c71676430
sanketb <sanketb@stud.ntnu.no>
2021-10-15 20:27:08 +0200

    added comments

--- Ardoq.py
+++ Ardoq.py
@@ -1,4 +1,5 @@
 
+#Using quicksort to sort the list
 def partition(list, low, high):
     i = (low-1)
     pivot = list[high]
@@ -19,7 +20,15 @@ def quicksort(list, low, high):
     return list
 
 def largestproduct(list):
+    if len(list) < 3:
+        return "List must have 3 or more numbers"
     list = quicksort(list, 0, len(list)-1)
+
+    #The largest product of three numbers in a sorted list must either be the first 2 numbers
+    #(because we can have negative numbers), and the last number in the list which is the largest number
+    #Or it must be the three last numbers in the list. Checking which one of these two possible outcomes are largest, and returning
+    #the largest of these 2
+
     prod1 = list[0]*list[1]*list[len(list)-1]
     prod2 = list[len(list)-1]*list[len(list)-2]*list[len(list)-3]
 
